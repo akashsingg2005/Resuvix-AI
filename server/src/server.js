caba6c32a5340aa307fb965fb236e1c1eea67291
Akash Kumar Singh <akashsingg2005@gmail.com>
2026-08-12 17:38:28 +0530

    fix: force IPv4 DNS resolution globally to resolve ENETUNREACH IPv6 routing error on Render

--- server/src/server.js
+++ server/src/server.js
@@ -1,3 +1,7 @@
+import dns from "dns";
+// Force DNS resolution to prefer IPv4 (fixes ENETUNREACH IPv6 routing issues on Render)
+dns.setDefaultResultOrder("ipv4first");
+
 import app from "./app.js";
 import connectDB from "./config/database.js";
 import env from "./config/env.js";
